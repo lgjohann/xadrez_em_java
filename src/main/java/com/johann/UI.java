95f6897cd62eb88621925ab386a4cfebd959e3eb
Guilherme Johann <guilherme4695@gmail.com>
2024-11-18 14:08:08 -0300

    Possible moves of a piece

--- src/main/java/com/johann/UI.java
+++ src/main/java/com/johann/UI.java
@@ -43,12 +43,12 @@ public class UI {
         }
     }
 
+
     // https://stackoverflow.com/questions/2979383/java-clear-the-console
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
-
 
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i = 0; i < pieces.length; i++) {
